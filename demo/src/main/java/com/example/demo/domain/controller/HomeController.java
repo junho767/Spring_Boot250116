663158d515ecf046969e5038a38b00a0fe69f52b
junho767 <junho767@naver.com>
2025-01-16 10:49:32 +0900

    article Json

--- demo/src/main/java/com/example/demo/domain/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/domain/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.example.demo.domain.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -27,4 +32,78 @@ public class HomeController {
         age++;
         return age;
     }
+
+    @GetMapping("/byte")
+    @ResponseBody
+    public byte getByte() {
+        return 1;
+    }
+
+    @GetMapping("/short")
+    @ResponseBody
+    public short getShort() {
+        return 1;
+    }
+
+    @GetMapping("/long")
+    @ResponseBody
+    public long getLong() {
+        return 1;
+    }
+
+    @GetMapping("/float")
+    @ResponseBody
+    public float getFloat() {
+        return 3.14f;
+    }
+
+    @GetMapping("/double")
+    @ResponseBody
+    public double getDouble() {
+        return 3.14;
+    }
+
+    @GetMapping("/char")
+    @ResponseBody
+    public char getChar() {
+        return '안';
+    }
+
+    @GetMapping("/boolean")
+    @ResponseBody
+    public boolean getBoolean() {
+        return true;
+    }
+
+    @GetMapping("/array")
+    @ResponseBody
+    public String[] getArray() {
+        return new String[]{"a", "b", "c"};
+    }
+
+    @GetMapping("/list")
+    @ResponseBody
+    public List<String> getList() {
+        return List.of("a", "b", "c");
+    }
+
+    @GetMapping("/map")
+    @ResponseBody
+    public Map<String, String> getMap() {
+        return Map.of("k1", "v1", "k2", "v2");
+    }
+
+    @GetMapping("/article")
+    @ResponseBody
+    public Article article() {
+        return new Article(1, "오늘은", "목요일");
+    }
+}
+
+@Getter
+@AllArgsConstructor
+class Article {
+    private int id;
+    private String title;
+    private String body;
 }
